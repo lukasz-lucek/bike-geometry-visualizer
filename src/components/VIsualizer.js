d5e05a1b07e3eb3d30ff1541438c9ad325c9eac8
lukasz-lucek <lukasz.lucek@gmail.com>
2023-08-09 22:44:34 +0200

    optimize visualizer not to replace image on every state change

--- src/components/VIsualizer.js
+++ src/components/VIsualizer.js
@@ -19,6 +19,9 @@ const Visualizer = forwardRef(({canvas}, ref) => {
 
   useImperativeHandle(ref, () => ({
     addNewLayer (layer) {
+        if (state.layers[0] && layer.src == state.layers[0].src) {
+          return;
+        }
         console.log("Visualizer.addNewLayer");
         updateState( {
             layers: [layer]
@@ -68,24 +71,84 @@ const Visualizer = forwardRef(({canvas}, ref) => {
 
   const addLayerToCanvas = (layer) => {
     if (layer.type === 'image') {
-      
-      fabric.Image.fromURL(layer.src, (img) => {
-        img.lockMovementX = true;
-        img.lockMovementY = true;
-        canvas.setZoom(Math.min(canvas.width / img.width, canvas.height / img.height));
-        canvas.insertAt(img, 0);
-        canvas.renderAll();
-        canvas.selection = false;
-        canvas.interactive = false;
-      });
+      if (layer.fabricObject == null) {
+        console.log("NEW IMAGE - resetting zoom")
+        canvas.setViewportTransform([1,0,0,1,0,0]);
+        fabric.Image.fromURL(layer.src, (img) => {
+          img.lockMovementX = true;
+          img.lockMovementY = true;
+          canvas.setZoom(Math.min(canvas.width / img.width, canvas.height / img.height));
+          canvas.insertAt(img, 0);
+          canvas.renderAll();
+          canvas.selection = false;
+          canvas.interactive = false;
+
+          layer.fabricObject = img;
+          updateState({layers:[layer]});
+        });
+      }
     }
   };
 
+  // const addShapeToCanvas = (key, shape, color, cumulativeStateUpdate) => {
+  //   if (!shape.fabricObject) {
+
+  //   }
+  //   if (shape.type == 'point') {
+  //     const circle = new fabric.Circle({
+  //       radius: 13,
+  //       fill: 'transparent', // Set the fill to transparent
+  //       stroke: color, // Set the stroke color
+  //       strokeWidth: 3, // Set the stroke width
+  //       selectable: false,
+  //       evented: false,
+  //       left: shape.x - 13,
+  //       top: shape.y - 13,
+  //     });
+
+  //     canvas.insertAt(circle, 1);
+  //   } else if (shape.type == 'line') {
+  //     const line = new fabric.Line(
+  //       [shape.x1, shape.y1, shape.x2, shape.y2],
+  //       {
+  //       stroke: color, // Set the stroke color
+  //       strokeWidth: 3, // Set the stroke width
+  //       selectable: false,
+  //       evented: false,
+
+  //     });
+
+  //     canvas.insertAt(line, 2);
+  //   }
+  // }
+
+  const isUsed = (fabricObject) => {
+    if (state.layers[0] && Object.is(state.layers[0].fabricObject, fabricObject)) {
+      return true;
+    }
+    for (const [, {shape: shape, color: color}] of Object.entries(state.overlayShapes)) {
+      if (shape && Object.is(shape.fabricObject, fabricObject)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const removeUnused = () => {
+    canvas.getObjects().forEach((fabricObject) => {
+      if (!isUsed(fabricObject)) {
+        console.log("removing unused fabric object ", fabricObject);
+        canvas.remove(fabricObject);
+      }
+    });
+  }
+
   useEffect(() => {
+    removeUnused();
     // remove all objects, before rendering new ones
-    canvas.remove(...canvas.getObjects());
+    //canvas.remove(...canvas.getObjects());
     //reset zooming
-    canvas.setViewportTransform([1,0,0,1,0,0]); 
+    //canvas.setViewportTransform([1,0,0,1,0,0]); 
     // Load initial layers to the canvas
     state.layers.forEach((layer) => addLayerToCanvas(layer));
 
