099a44497497318332f6fd212b7b8c219f34b87b
lukasz-lucek <lukasz.lucek@gmail.com>
2024-03-03 01:10:32 +0100

    copy client build to server dist for cyclic to be able to see client files

--- server/src/index.ts
+++ server/src/index.ts
@@ -41,11 +41,11 @@ const corsOptionsDelegate = (req : any, callback : any) => {
 }
 app.use(cors(corsOptionsDelegate));
 
-app.use(express.static(path.join(__dirname, "../../client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get('/', (req, res) => {
   res.sendFile(
-    path.join(__dirname, "../../client/build/index.html"),
+    path.join(__dirname, "client/build/index.html"),
     (err) => {
       res.status(500).send(err);
     }
